c8a75808c9ee9d567307e500819882a45f0f10a8
Thanatisia <55834101+Thanatisia@users.noreply.github.com>
2024-04-01 14:11:33 +0800

    Update 2024-04-01 1340H - New     + Added new python packaging script 'setup.py' for setuptools     + Added '.gitignore'

--- CHANGELOGS.md
+++ CHANGELOGS.md
@@ -8,10 +8,20 @@
 #### 1303H
 + Initial Commit
 
++ Version: 0.1.0
+
 - New
     + Added new document 'README.md'
     + Added new document 'CHANGELOGS.md'
     + Added new document 'USAGE.md'
     - Added new package 'gh-utils'
         + Added new module 'gh-clone.py' : Clones git repositories from GitHub based on a list of your GitHub repositories (presumable obtained via the GitHub API)
+
+
+#### 1340H
+- New
+    + Added new python packaging script 'setup.py' for setuptools
+    + Added '.gitignore'
+
+
 
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gh-utils',
+    version='0.1.0',
+    description="Collection of various GitHub-related git remote repository server helper utilities",
+    author='Thanatisia',
+    author_email='55834101+Thanatisia@users.noreply.github.com',
+    # packages=["mkparse"] ,# Default: find_packages()
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=[
+        # List your dependencies here
+    ],
+    url='https://github.com/Thanatisia/gh-utils',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    entry_points = {
+        # Program Entry Point(s) and scripts
+        'console_scripts' : [
+            'gh-clone = gh-utils/gh-clone:main',
+        ],
+    },
+)
+
